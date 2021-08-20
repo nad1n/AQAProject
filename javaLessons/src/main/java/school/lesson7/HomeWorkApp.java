b1f1a6c72f2b09327de3e36e05342733c6f0250f
Nadiia Aleksieienko <petrenko.nad1n@gmail.com>
2021-08-20 14:13:52 +0300

    lesson7 Commit

--- javaLessons/src/main/java/school/lesson7/HomeWorkApp.java
+++ javaLessons/src/main/java/school/lesson7/HomeWorkApp.java
@@ -7,9 +7,7 @@ public class HomeWorkApp {
         int[][] dataCell = new int[][]{{1, 32, 635}, {2, 64, 999}, {3, 128, 1244}, {4, 256, 1678}};
 
         AppData appData = new AppData(nameColumn, dataCell);
-
         appData.save("file.cvs"); // Реализовать сохранение данных в csv файл;
         appData.read("file.cvs"); //   Реализовать загрузку данных из csv файла. Файл читается целиком.
-
     }
 }
